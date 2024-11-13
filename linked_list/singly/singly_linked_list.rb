90c2f677d24e6bf64655aae4ef80f6aa3f508604
Jii Yoo <jiiyoo@tuta.io>
2024-11-13 12:36:21 -0800

    singly linked list

--- linked_list/singly/singly_linked_list.rb
+++ linked_list/singly/singly_linked_list.rb
@@ -4,7 +4,7 @@ class SinglyLinkedList
     class Node
         attr_accessor :data, :next
 
-        def initialize  data, pointer
+        def initialize  data=nil, pointer=nil
             @data = data
             @next = pointer
         end
@@ -20,6 +20,7 @@ class SinglyLinkedList
         node = Node.new data 
         unless head
             self.head = node
+            self.head.next = self.tail
         else
             self.tail.next = node
         end
@@ -30,18 +31,26 @@ class SinglyLinkedList
     def remove node
         return nil unless node
 
-        if node == head
-            if head.next.nil?
+        if node == self.head
+            if self.head.next.nil?
                 self.head = self.tail = nil
             else
                 self.head = self.head.next
             end
+        elsif node == self.tail
+            tmp = self.head
+            while tmp && tmp.next != self.tail
+                tmp = tmp.next
+            end
+            tmp.next = nil
+            self.tail = tmp
         else
             tmp = self.head
             while tmp && tmp.next != node
                 tmp = tmp.next
             end
             tmp.next = node.next if tmp
+            self.tail = tmp
         end
         self.length -= 1
     end
@@ -51,6 +60,8 @@ class SinglyLinkedList
 
         self.tail.next = list.head
         self.length += list.length
+
+        self.tail = list.tail
     end
 
     def clear
@@ -59,9 +70,39 @@ class SinglyLinkedList
         end
     end
 
-    def find_first predicate
+    def find_first &predicate
+        return nil unless block_given?
+
+        current = self.head
+        while current
+            return current if predicate.call(current)
+            current = current.next
+        end
     end
 
-    def print
+    def each
+        return nil unless block_given?
+
+        current = self.head
+        while current
+            yield current
+            current = current.next
+        end
+    end
+
+    def display
+        if self.length == 0
+            puts "empty"
+        else
+            self.each { |item| puts item.data } 
+        end
     end
 end
+
+list = SinglyLinkedList.new
+list.insert(1)
+list.insert(2)
+list.insert(3)
+list.insert(4)
+list.insert(5)
+list.display
