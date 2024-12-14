2357acb16ff406898cfd49cfef486ee6accf6d4f
Jii Yoo <jiiyoo@tuta.io>
2024-12-14 10:49:05 -0800

    binary tree

--- binary_tree/binary_tree.rb
+++ binary_tree/binary_tree.rb
@@ -1,19 +1,58 @@
 class BinaryTree
-    def initialize
-        self.size = 0
-        @root     = nil
+    attr_accessor :root, :size
+
+    class Node
+        attr_accessor :parent, :left, :right, :data
+
+        def initialize parent, data
+            self.parent = parent
+            self.data = data
+        end
+
+        def to_s
+            puts self.data
+            puts self.left
+            puts self.right
+        end
+    end
+
+    def initialize root
+        self.root = Node.new nil, root
+        self.size = 1
     end
 
     def insert_left node, data
+        return unless node
+        raise "can't override nodes." if node.left
+
+        self.size += 1
+        node.left = Node.new node, data
     end
 
-    def insert_right node, dataa
+    def insert_right node, data
+        return unless node
+        raise "can't override nodes." if node.right
+
+        self.size += 1
+        node.right = Node.new node, data
     end
 
     def remove_left node
+        if node&.left
+            remove_left node.left
+            remove_right node.left
+            node.left = nil
+            self.size -= 1
+        end
     end
-
-    def remove_right node
+    
+    def remove_right node    
+        if node&.right    
+            remove_left node.right
+            remove_right node.right
+            node.right = nil
+            self.size -= 1
+        end
     end
 
     def merge left, right
@@ -25,15 +64,4 @@ class BinaryTree
     def print_bfs
     end
 
-    private
-    class Node
-        attr_accessor :parent, :left, :right, data
-
-        def initialize data, parent=nil, left=nil, right=nil
-            self.parent = parent
-            self.left = left
-            self.right = right
-            self.data = data
-        end
-    end
 end
